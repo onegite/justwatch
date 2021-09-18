302d55d0ef76946265b175359b3803f4c02f9ba3
onegite <90951596+onegite@users.noreply.github.com>
2021-09-18 15:13:59 +0700

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'JustWatch'
+copyright = '2021, JustWatch'
+author = 'JustWatch'
 
 release = '0.1'
 version = '0.1.0'
